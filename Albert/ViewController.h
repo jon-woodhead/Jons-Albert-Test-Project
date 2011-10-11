77ed9292671fb5fad62864693b319cf6294b93ad
Digital Sol <digitalsol@acad0535.ipt.aol.com>
2011-10-11 09:54:51 +0100

    Initial Commit

---
+++ Albert/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Albert
+//
+//  Created by Digital Sol on 11/10/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
