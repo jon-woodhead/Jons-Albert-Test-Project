66d5df92a8e814035b47af83c345f342998e8280
Jon Woodhead <jon_woodhead@hotmail.com>
2014-03-26 09:37:42 +0000

    do commit

--- Albert/AppDelegate.m
+++ Albert/AppDelegate.m
@@ -23,11 +23,13 @@
 							
 - (void)applicationWillResignActive:(UIApplication *)application
 {
-    NSLog(@"Goodbye Matt");
+    NSLog(@"Goodbye Mathew");
     /*
      Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
      Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
      */
+    
+     NSLog(@"Goodbye pung");
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
