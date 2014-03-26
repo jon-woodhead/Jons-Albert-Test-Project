4256b330b7976f51151852491b9d9f93994e3235
Jon Woodhead <jon_woodhead@hotmail.com>
2014-03-26 10:51:16 +0000

    Thingy

--- Albert/AppDelegate.m
+++ Albert/AppDelegate.m
@@ -16,7 +16,7 @@
 {
     
     NSLog(@"Hello Jon    !");
-     NSLog(@"What a fine elephant  dingo");
+     NSLog(@"What a fine elephant breath dingo");
     // Override point for customization after application launch.
     return YES;
 }
